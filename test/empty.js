51e01c347424f58caaa55563ad7dcde3b1d5b727
James Halliday <mail@substack.net>
2015-09-27 11:28:23 -0700

    failing empty test

---
+++ test/empty.js
@@ -0,0 +1,11 @@
+var test = require('tape')
+var fs = require('fs')
+var hver = require('../')
+var cheerio = require('cheerio')
+
+var src = fs.readFileSync(__dirname + '/data/src.html', 'utf8')
+
+test('empty upgrade', function (t) {
+  t.plan(4)
+  t.equal(hver.update(src, [], {}), src)
+})
